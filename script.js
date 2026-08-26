ac68e51f7eabc60ffae09e6a83b98cfb178a135d
Julia Artigas <julia.artigas.castro@escola.pr.gov.br>
2026-08-25 23:22:02 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Arquivo de apoio. O foco pedagógico deste exercício é a semântica do HTML.
-document.querySelectorAll('nav a').forEach((link) => {
-  link.addEventListener('click', () => {
-    document.querySelectorAll('nav a[aria-current]').forEach((item) => item.removeAttribute('aria-current'));
-    link.seAttribute('aria-current', 'location');
-  });
-});
-console.info('Exercício 01 carregado.');
